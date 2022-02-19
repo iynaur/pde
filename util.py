4a4bc3f5171c0270027716060852c47881311540
Ruan Yi <iynaur87@sina.com>
2022-02-19 11:30:04 +0800

    reduce mem copy

--- util.py
+++ util.py
@@ -9,6 +9,7 @@ import cv2
 import sys, os
 from multiprocessing import Pool
 import numba
+from itertools import repeat
 
 
 def ifft_ups(fft, ups):
@@ -16,11 +17,12 @@ def ifft_ups(fft, ups):
     crow, ccol = fft.shape
     om = cmath.exp(2*pi/crow * (0+1j) )
     on = cmath.exp(2*pi/ccol * (0+1j) )
+    rfft = np.zeros((crow*ups, ccol* ups), dtype=np.complex_)
 
     @numba.jit(nogil=True)
-    def proc(ii):
+    def proc(ii, rfft):
         i = ii/ups
-        rfft = np.zeros(ccol* ups, dtype=np.complex_)
+        # rfft = np.zeros(ccol* ups, dtype=np.complex_)
         for jj in range(0, ccol* ups, 1):
             j = jj/ups
             for p in range(crow):
@@ -28,11 +30,11 @@ def ifft_ups(fft, ups):
                     # better interpolate
                         nnp = p if p < crow -p else p - crow
                         nq = q if q<  ccol - q else q - ccol
-                        rfft[jj] += fft[p, q] * om ** (nnp*i) * on ** (nq*j)
-        return rfft
+                        rfft[ii, jj] += fft[p, q] * om ** (nnp*i) * on ** (nq*j)
+        return 0
     with ThreadPoolExecutor(max_workers=4) as executor:
-        ite = executor.map(proc, range(0, crow* ups, 1))
-        rfft = np.array(list(ite))
+        ite = executor.map(proc, range(0, crow* ups, 1), repeat(rfft))
+        # rfft = np.array(list(ite))
     # plt.imshow(np.real(rfft))
     # plt.show(block=1)
     return rfft
